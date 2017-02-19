86c6e61ee94bf538a3a3ea18e2f20d0e659b8b3e
raffa <raffalevy@me.com>
2017-02-18 22:57:10 -0500

    copyrights

--- src/com/raphaellevy/math/visualizer/Visualization.java
+++ src/com/raphaellevy/math/visualizer/Visualization.java
@@ -1,3 +1,6 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Raffa Levy. All rights reserved.
+ *******************************************************************************/
 package com.raphaellevy.math.visualizer;
 
 import java.awt.Dimension;
