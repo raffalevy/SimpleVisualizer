24aa5044564a56f20e4f710a288e9356c2c14f0f
raffa <raffalevy@me.com>
2017-02-19 09:03:02 -0500

    added color, synchronization, zoom

--- src/com/raphaellevy/math/visualizer/GridVisualization.java
+++ src/com/raphaellevy/math/visualizer/GridVisualization.java
@@ -11,16 +11,21 @@ import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
  * A configurable grid/graph onto which lines and shapes can be drawn.
  * @author raffa
  */
-public class GridVisualization extends Visualization{
+public final class GridVisualization extends Visualization{
 	private Visualizer visualizer;
 	private JPanel panel;
 	
@@ -38,7 +43,7 @@ public class GridVisualization extends Visualization{
 	 * Gets the visualizer.
 	 */
 	@Override
-	protected Visualizer getVisualizer() {
+	protected synchronized Visualizer getVisualizer() {
 		return visualizer;
 	}
 
@@ -46,31 +51,60 @@ public class GridVisualization extends Visualization{
 	 * Gets the panel.
 	 */
 	@Override
-	public JPanel getPanel() {
+	public synchronized JPanel getPanel() {
 		return panel;
 	}
 	
 	private double scaleFactor = 1;
+	
 	/**
 	 * Sets how much to zoom into the grid.
 	 * @param sf
 	 */
-	public void setScaleFactor(double sf) {
+	public synchronized void setScaleFactor(double sf) {
 		scaleFactor=sf;
+		panel.repaint();
+	}
+	
+	/**
+	 * Gets the current scaling factor for the grid
+	 */
+	public synchronized double getScaleFactor() {
+		return scaleFactor;
+	}
+	
+	/**
+	 * Multiplies the current scaling factor by a given amount.
+	 * @param factor
+	 */
+	public synchronized void zoom(double factor) {
+		scaleFactor = scaleFactor * factor;
+		panel.repaint();
 	}
 	
 	private int unit = 10;
 	/**
-	 * sets how wide each grid square should be.
+	 * sets how wide in pixels (before scaling) each grid square should be.
 	 * @param unit
 	 */
-	public void setUnit(int unit) {
+	public synchronized void setUnit(int unit) {
 		this.unit = unit;
+		panel.repaint();
 	}
+	
+	/**
+	 * Gets the current grid unit, in pixels (before scaling)
+	 * @return
+	 */
+	public synchronized int getUnit() {
+		return unit;
+	}
+	
+	private Stack<Color> colorStack = new Stack<>();
 	
 	private class GridPanel extends JPanel{
 		@Override
-		public void paint(Graphics g1) {
+		public synchronized void paint(Graphics g1) {
 			super.paint(g1);
 			Graphics2D g = (Graphics2D) g1;
 			AffineTransform save = new AffineTransform(g.getTransform());
@@ -86,23 +120,66 @@ public class GridVisualization extends Visualization{
 			
 			g.transform(tr);
 			
-			for (Rectangle r : gridRectangles) {
+			drawAxes(g);
+			for (Rectangle r : gridRectangles.keySet()) {
+				colorStack.push(g.getColor());
+				g.setColor(gridRectangles.get(r));
 				g.fill(AffineTransform.getScaleInstance(unit, unit).createTransformedShape(r));
+				g.setColor(colorStack.pop());
 			}
-			for (Line2D l : gridLines) {
+			for (Line2D l : gridLines.keySet()) {
+				colorStack.push(g.getColor());
+				g.setColor(gridLines.get(l));
 				g.draw(AffineTransform.getScaleInstance(unit, unit).createTransformedShape(l));
+				g.setColor(colorStack.pop());
 			}
 			
 			g.setTransform(save);
 		}
+		@Override
+		public void repaint() {
+			JPanel p = this;
+			if (SwingUtilities.isEventDispatchThread()) {
+				super.repaint();
+			} else {
+				try {
+					SwingUtilities.invokeAndWait(new Runnable() {
+						@Override
+						public void run() {
+							p.repaint();
+						}
+					});
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
-	private List<Rectangle> gridRectangles = new ArrayList<>();
-	private List<Line2D> gridLines = new ArrayList<>();
+	private Map<Rectangle,Color> gridRectangles = new HashMap<>();
+	private Map<Line2D,Color> gridLines = new HashMap<>();
+	
+	private Color defaultObjectColor = Color.DARK_GRAY;
+	
+	/**
+	 * Sets the default color for objects added to the grid.
+	 * @param c
+	 */
+	public synchronized void setDefaultObjectColor(Color c) {
+		defaultObjectColor = c;
+	}
+	
+	/**
+	 * Gets the default color for objects added to the grid.
+	 */
+	public synchronized Color getDefaultObjectColor() {
+		return defaultObjectColor;
+	}
 	
 	/**
 	 * clears all shapes from the grid.
 	 */
-	public void clear() {
+	public synchronized void clear() {
 		gridRectangles.clear();
 		gridLines.clear();
 		panel.repaint();
@@ -112,8 +189,18 @@ public class GridVisualization extends Visualization{
 	 * Add a rectangle to be painted to the grid. Uses grid units.
 	 * @param rect
 	 */
-	public void addGridRectangle(Rectangle rect) {
-		gridRectangles.add(rect);
+	public synchronized  void addGridRectangle(Rectangle rect) {
+		gridRectangles.put(rect, defaultObjectColor);
+		panel.repaint();
+	}
+	
+	/**
+	 * Add a rectangle to be painted to the grid. Uses grid units.
+	 * @param rect
+	 * @param c The color of the rectangle
+	 */
+	public synchronized void addGridRectangle(Rectangle rect, Color c) {
+		gridRectangles.put(rect, c);
 		panel.repaint();
 	}
 	
@@ -121,8 +208,18 @@ public class GridVisualization extends Visualization{
 	 * Add a line to be painted to the grid. Uses grid units.
 	 * @param line
 	 */
-	public void addGridLine(Line2D line) {
-		gridLines.add(line);
+	public synchronized void addGridLine(Line2D line) {
+		gridLines.put(line, defaultObjectColor);
+		panel.repaint();
+	}
+	
+	/**
+	 * Add a line to be painted to the grid. Uses grid units.
+	 * @param line
+	 * @param c The color of the line
+	 */
+	public synchronized void addGridLine(Line2D line, Color c) {
+		gridLines.put(line, c);
 		panel.repaint();
 	}
 	
@@ -131,14 +228,60 @@ public class GridVisualization extends Visualization{
 	/**
 	 * Centers the grid on the specified point.
 	 */
-	public void centerOnGridPoint(Point2D p) {
+	public synchronized void centerOnGridPoint(Point2D p) {
 		tr.translate(-p.getX()*10, -p.getY()*10);
+		panel.repaint();
 	}
 	
 	/**
 	 * Sets the grid to normal center.
 	 */
-	public void reCenter() {
+	public synchronized void reCenter() {
 		tr = new AffineTransform();
+	}
+	
+	private boolean axesShown;
+	private Color axesColor = Color.black;
+	
+	/**
+	 * Sets whether to show x and y axes.
+	 * @param b
+	 */
+	public synchronized void showAxes(boolean b) {
+		axesShown = b;
+		panel.repaint();
+	}
+	
+	/**
+	 * Set the color of the axes
+	 * @param color
+	 */
+	public synchronized void setAxesColor(Color color) {
+		axesColor = color;
+		if (axesShown) {
+			panel.repaint();
+		}
+	}
+	
+	/**
+	 * Set the color of the axes
+	 * @param red 0-255
+	 * @param green 0-255
+	 * @param blue 0-255
+	 */
+	public synchronized void setAxesColor(int red, int green, int blue) {
+		setAxesColor(new Color(red, green, blue));
+	}
+	
+	private void drawAxes(Graphics2D g) {
+		if (axesShown) {
+			colorStack.push(g.getColor());
+			g.setColor(axesColor);
+			int wid = panel.getGraphicsConfiguration().getBounds().width;
+			int hei = panel.getGraphicsConfiguration().getBounds().height;
+			g.drawLine(-wid, 0, wid, 0);
+			g.drawLine(0, -hei, 0, hei);
+			g.setColor(colorStack.pop());
+		}
 	}
 }
--- src/com/raphaellevy/math/visualizer/Visualizer.java
+++ src/com/raphaellevy/math/visualizer/Visualizer.java
@@ -4,44 +4,48 @@
 package com.raphaellevy.math.visualizer;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
  * Represents a visualization application and contains preferences.
+ * 
  * @author raffa
  */
 public class Visualizer {
 	static final int DEFAULT_WINDOW_WIDTH = 730;
 	static final int DEFAULT_WINDOW_HEIGHT = 470;
-	
+
 	public static final boolean Y_UP = true;
 	public static final boolean Y_DOWN = false;
-	
+
 	/**
-	 * Get a new Visualizer. This should be the first SimpleVisualizer method called in your application.
+	 * Get a new Visualizer. This should be the first SimpleVisualizer method
+	 * called in your application.
 	 */
 	public static Visualizer open() {
 		return new Visualizer();
 	}
-	
+
 	private JFrame frame;
-	
+
 	private int windowWidth = DEFAULT_WINDOW_WIDTH;
 	private int windowHeight = DEFAULT_WINDOW_HEIGHT;
 	private boolean yPreference = Y_UP;
 	private boolean centerVisualization = true;
-	
+
 	private Visualizer() {
-		
+
 	}
-	
+
 	/**
 	 * Set the starting width of the window.
+	 * 
 	 * @param width
 	 */
 	public void setWindowWidth(int width) {
 		windowWidth = width;
 	}
-	
+
 	/**
 	 * @return The current window width.
 	 */
@@ -53,15 +57,16 @@ public class Visualizer {
 		}
 		return windowWidth;
 	}
-	
+
 	/**
 	 * Set the starting height of the window.
+	 * 
 	 * @param height
 	 */
 	public void setWindowHeight(int height) {
 		windowHeight = height;
 	}
-	
+
 	/**
 	 * @return The current window height.
 	 */
@@ -73,55 +78,75 @@ public class Visualizer {
 		}
 		return windowHeight;
 	}
-	
+
 	/**
-	 * Either {@link Visualizer#Y_UP} or {@link Visualizer#Y_DOWN}. 
-	 * If Y_UP, uses math-like behavior with the y axis going up. 
-	 * If Y_DOWN, uses computer behavior with the y axis going down. 
+	 * Either {@link Visualizer#Y_UP} or {@link Visualizer#Y_DOWN}. If Y_UP,
+	 * uses math-like behavior with the y axis going up. If Y_DOWN, uses
+	 * computer behavior with the y axis going down.
+	 * 
 	 * @param pref
 	 */
 	public void setYPreference(boolean pref) {
 		yPreference = pref;
 	}
-	
+
 	/**
-	 * Get current Y preference; see {@link Visualizer#setYPreference setYPreference()}.
+	 * Get current Y preference; see {@link Visualizer#setYPreference
+	 * setYPreference()}.
+	 * 
 	 * @return
 	 */
 	public boolean getYPreference() {
 		return yPreference;
 	}
-	
+
 	/**
 	 * @return The application's JFrame.
 	 */
 	public JFrame getFrame() {
 		return frame;
 	}
-	
+
 	/**
 	 * If true, places origin at center of window rather than corner.
+	 * 
 	 * @param b
 	 */
 	public void setCenterVisualization(boolean b) {
 		centerVisualization = b;
 	}
-	
+
 	/**
-	 * @return wheter the visualization is centered; see {@link Visualizer#setCenterVisualization(boolean)}
+	 * @return whether the visualization is centered; see
+	 *         {@link Visualizer#setCenterVisualization(boolean)}
 	 */
 	public boolean visualizationCentered() {
 		return centerVisualization;
 	}
-	
+
 	/**
 	 * Displays the given visualization in a new frame.
+	 * 
 	 * @param v
 	 */
-	public void displayFrame(Visualization v) {
-		frame = new JFrame();
-		frame.setContentPane(v.getPanel());
-		frame.pack();
-		frame.setVisible(true);
+	public synchronized void displayFrame(Visualization v) {
+		if (frame != null) {
+			RuntimeException e = new RuntimeException("There already is a frame!");
+			throw e;
+		}
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {
+				@Override
+				public void run() {
+					frame = new JFrame();
+					frame.setContentPane(v.getPanel());
+					frame.pack();
+					frame.setVisible(true);
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
